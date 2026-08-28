d790a8a0cf09dd58fbcf6280a792841ea2804ced
Zitang <freemathslessons123@gmail.com>
2026-08-28 15:16:42 +0100

    Add blackjack and optimal stopping simulators

---
+++ optimal_stopping_simulator.py
@@ -0,0 +1,26 @@
+import random
+
+T = int(input("Enter threshhold between 1 and 100: "))
+max_rolls = int(input("Maximum number of rolls: "))
+
+count = 0
+final = 0
+
+while count < max_rolls:
+    num = random.randint(1, 100)
+    count += 1
+
+    if count == max_rolls:
+        print(f"Roll {count}: {num} - Forced stop")
+        final = count
+        break
+
+    if num >= T:
+        print(f"Roll {count}: {num} - Stop")
+        final = count
+        break
+
+    print(f"Roll {count}: {num} - Continue")
+
+print(f"Final Score: {final}")
+print(f"Rolls: {count}")
